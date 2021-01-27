7c6b40b9a928690555ee297dcbae4bc759b38d30
Pablo <pablo_ortega@es.ibm.com>
2021-01-27 16:41:21 +0100

    falta -

--- install.py
+++ install.py
@@ -1,2 +1,2 @@
-AdminApp.install('/tmp/web-app-2_1.0-SNAPSHOT.war', '-server server1 -contextroot web-app-2 defaultbinding.virtual.host default_host -usedefaultbindings')
+AdminApp.install('/tmp/web-app-2_1.0-SNAPSHOT.war', '-server server1 -contextroot web-app-2 -defaultbinding.virtual.host default_host -usedefaultbindings')
 AdminConfig.save()
