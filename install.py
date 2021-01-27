1bd138af71f7eeef22aae5d84e1d383ec50654a2
Pablo <pablo_ortega@es.ibm.com>
2021-01-27 11:07:49 +0100

    Parámetros de instlación

--- install.py
+++ install.py
@@ -1,1 +1,1 @@
-AdminApp.install('/tmp/web-app-2_1.0-SNAPSHOT.war')
+AdminApp.install('/tmp/web-app-2_1.0-SNAPSHOT.war', '-server server1 -defaultbinding.virtual.host default_host -usedefaultbindings')
